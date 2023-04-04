5f106c2b864aa3cc0720a88a5061ce787de52fdc
Ryan Eastman <rking1323@gmail.com>
2023-04-04 19:34:50 +0000

    cleared up whitespace

--- modules/weather.js
+++ modules/weather.js
@@ -4,11 +4,6 @@ const axios = require('axios');
 
 let cache = {};
 
-//TODO: need to create a key for the data we are going to store
-//TODO: if the thing exists AND within a valid timeframe, send data from cache
-// TODO: if the thing does NOT exist, call API and cache that return from API
-
-
 const apiKey = process.env.STORM_GLASS_API;
 
 async function getWeatherData(req, res, next) {
@@ -30,11 +25,6 @@ async function getWeatherData(req, res, next) {
         method: 'get',
         baseURL: `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${params}`
       };
-      // let waveData = await axios.get(url, {
-      //   headers: {
-      //     'Authorization': apiKey
-      //   }
-      // });
 
       let waveData = await axios(config);
 
@@ -54,38 +44,10 @@ async function getWeatherData(req, res, next) {
   }
 }
 
-// console.log('URL', url);
-// const returnedWeatherData = response.data;
-// console.log('Axios Returned Data:', returnedWeatherData);
 
 module.exports = getWeatherData;
 
 
 
-// if (cache[key] && (Date.now() - cache[key].timestamp) < 7.884e+9) {
-  
-  //   response.status(200).send(cache[key].data);
-  
-  // } else {
-    
-    //   let weatherInfoUrl = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}&days=7&units=I`
-    
-    //   let axiosWeatherInfo = await axios.get(weatherInfoUrl);
-    //   let weatherInfo = axiosWeatherInfo.data.data.map(day => new Forecast(day));
-    
-    //   cache[key] = {
-      //     data: weatherInfo,
-      //     timestamp: Date.now()
-      //   };
-      
-      
-      
-      //  { headers: {
-      //     'Authorization': apiKey
-      // }
-      // });
-      // const response = await axios.get(url, { headers: 'Authorization': apiKey });
-      // }).then((response) => response.json()).then((jsonData) => {
-      //   console.log(jsonData);
-      //   console.log(url);
-      
+
+
